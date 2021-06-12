401f3b5fa1fcac52a91624680360f975364b0535
Chandra Lama <iamme24cl@gmail.com>
2021-06-12 12:55:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,31 @@
 // Code your solution here!
 
+// Write a recursive function to print out all of the characters in a string
+
+function  printString(string) {
+  console.log(string[0]);
+
+  if (string.length > 1) {
+    let subString = string.substring(1, string.length);
+    printString(subString);
+  } else {
+    return true;
+  }
+}
+
+// Write out a recursive function to reverse a string
+
+function reverseString(string) {
+  return string ? reverseString(string.substr(1)) + string[0] : string;
+}
+
+// Write out a recursive fucntion to see if a word is a Palindrome
+
+function isPalindrome(string) {
+  // string = string.replace(/[^a-z0-9]/i, '').toLowerCase();
+
+  if (string.length <= 1) return true;
+  if (string[0] !== string[string.length -1]) return false;
+
+  return isPalindrome(string.slice(1, -1));
+}
